171aef244d31ef343596c0145650a487d41c04b1
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2015-04-07 14:27:19 -0400

    add gerkin for user spec

--- spec/feature/users_spec.rb
+++ spec/feature/users_spec.rb
@@ -3,4 +3,11 @@ require 'rails_helper'
 feature 'Users' do
   # create a new user
   # login
+  
+
+  # Given I am on the home page
+  # When I enter my username and password 
+  # And click login
+  # Then I expect to be logged in
+
 end

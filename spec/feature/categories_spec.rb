540dc3f1bb40f5a22866b2a6849521b1efa878d7
Mary Baylis <mary.c.baylis@gmail.com>
2015-04-07 14:51:41 -0400

    Pseudo test

--- spec/feature/categories_spec.rb
+++ spec/feature/categories_spec.rb
@@ -6,4 +6,13 @@ feature 'Browsing categories' do
   # GIVEN: A list of articles exists
   # WHEN: I click on an article link
   # THEN: I expect the browser to display the article's details
+
+  it "shows an article" do
+  	c = Category.make(name: "Shenanigans")
+  	au = Author.make(username: "author", password: "author", email:"")
+  	ar = Article.make(title: "Stuff", description: "This is stuff", price: 8, author: au, category: c)
+  	visit category_path(c)
+  	click_link('Stuff')
+  	expect(page).to have_content("This is stuff.")
+  end
 end

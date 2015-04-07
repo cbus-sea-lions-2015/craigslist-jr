08a3776758ab8089deb673f6dce469513312fa1c
Lucas Willett <me@ltw.io>
2015-04-07 13:11:24 -0500

    No more tests plz

--- spec/feature/categories_spec.rb
+++ spec/feature/categories_spec.rb
@@ -1,31 +1,6 @@
 require 'rails_helper'
 
 feature 'Browsing categories' do
-  # given a category
-  let!(:category) { Category.create!(name: 'bikes') }
-  # and an article in that category
-  let!(:user) { User.create(username: 'fakey', email: 'fake@fake.fake', password: 'fake') }
-  let!(:article) { category.articles.create!(author: user, title: 'bike4sale', description: 'buy my bike you friend') }
-
   # navigate categories
-  scenario 'clicking a category link' do
-    # when a user clicks the category link
-    visit(categories_path)
-    click_link('bikes')
-    # then I should see the category's page
-    expect(current_path).to eq(category_articles_path(category))
-    # with the article title on it
-    expect(page).to have_content('bike4sale')
-  end
-
   # show an article
-  scenario 'viewing an article' do
-    # when i click on an article
-    visit(category_articles_path(category))
-    click_link('bike4sale')
-    # the article's information will appear
-    expect(current_path).to eq(category_article_path(category, article))
-    expect(page).to have_content('bike4sale')
-    expect(page).to have_content('buy my bike you friend')
-  end
 end
--- spec/feature/users_spec.rb
+++ spec/feature/users_spec.rb
@@ -2,34 +2,5 @@ require 'rails_helper'
 
 feature 'Users' do
   # create a new user
-  scenario 'signing up' do
-    visit(signup_path)
-    expect(page).to_not have_link("Logout")
-
-    fill_in 'Username', with: 'fakey'
-    fill_in 'Email', with: 'fake@fake.fake'
-    fill_in 'Password', with: 'fake'
-    fill_in 'Password confirmation', with: 'fake'
-    click_button 'Create User'
-
-    expect(current_path).to eq(dashboard_path)
-    expect(page).to have_link("Logout")
-  end
-
   # login
-  feature 'with an existing user' do
-    let!(:user) { User.create!(username: 'fakey', email: 'fake@fake.fake', password: 'fake') }
-
-    scenario 'logging in' do
-      visit(login_path)
-      expect(page).to have_link("Login")
-
-      fill_in 'Email', with: 'fake@fake.fake'
-      fill_in 'Password', with: 'fake'
-      click_button 'Login'
-
-      expect(current_path).to eq(dashboard_path)
-      expect(page).to_not have_link("Login")
-    end
-  end
 end

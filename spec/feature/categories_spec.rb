cc1854205662b9d5ef0a81036da42380db55aca9
Jose Roman <jferoman@gmail.com>
2015-04-07 14:52:20 -0400

    add capybara test caterories

--- spec/feature/categories_spec.rb
+++ spec/feature/categories_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
+given(:CAT) { Category.create(:name => "dogs")}
+
 feature 'Browsing categories' do
+ # given: that categories exist
  # when:  the user visits the categories page.
- # then:  A list of containing all the categories should display. 
+ # then:  A list of containing all the categories should display.
+
+ scenario "should display a list with all existing categories" do 
+  
+  visit '/categories' 
+  expect(page).to have_content(category.name)
+
+ end
+
+
 end

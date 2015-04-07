72c5423da7f692aafc620c51228bac1a1567331c
Mary Baylis <mary.c.baylis@gmail.com>
2015-04-07 14:29:02 -0400

    I AM NOT STALE, I SWEAR

--- spec/feature/categories_spec.rb
+++ spec/feature/categories_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 feature 'Browsing categories' do
   # navigate categories
   # show an article
+  # GIVEN: A list of articles exists
+  # WHEN: I click on an article link
+  # THEN: I expect the browser to display the article's details
 end

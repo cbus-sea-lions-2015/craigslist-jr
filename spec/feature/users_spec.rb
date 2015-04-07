c96ba91b12cb745df2abc737f713bfc584a50c65
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2015-04-07 14:54:54 -0400

    add test for login

--- spec/feature/users_spec.rb
+++ spec/feature/users_spec.rb
@@ -6,8 +6,15 @@ feature 'Users' do
   
 
   # Given I am on the home page
+  visit('/login')
   # When I enter my username and password 
+  within('#login-form') do
+    fill_in('Email', :with => 'batman@batman.com')
+    fill_in('Password', :with => 'batman')
+    click_button('Login')
+  end
   # And click login
-  # Then I expect to be logged in
 
+  # Then I expect to be logged in
+  page.has_content?('Bruce')
 end
